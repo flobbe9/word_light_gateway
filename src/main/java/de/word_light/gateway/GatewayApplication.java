55bc9596468420dfd3a3954ab10fe7f9d44b80f0
flobbe9 <florin735@live.com>
2024-02-02 12:53:12 +0100

    revert: get rid of ssl

--- src/main/java/de/word_light/gateway/GatewayApplication.java
+++ src/main/java/de/word_light/gateway/GatewayApplication.java
@@ -3,14 +3,13 @@ package de.word_light.gateway;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
-import de.word_light.gateway.config.ApplicationInitializer;
-
 
 @SpringBootApplication
 public class GatewayApplication {
 
 	public static void main(String[] args) {
-        new ApplicationInitializer(args).init();
+
+        // new ApplicationInitializer(args).init();
 		SpringApplication.run(GatewayApplication.class, args);
 	}
 }
