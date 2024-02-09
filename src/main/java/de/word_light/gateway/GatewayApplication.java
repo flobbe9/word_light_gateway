fac0185e8f6d5af0b2a499f5678a2080328d821f
flobbe9 <florin735@live.com>
2024-02-09 12:23:08 +0100

    minor: log version on startup

--- src/main/java/de/word_light/gateway/GatewayApplication.java
+++ src/main/java/de/word_light/gateway/GatewayApplication.java
@@ -1,7 +1,14 @@
 package de.word_light.gateway;
 
+import java.io.FileInputStream;
+import java.io.IOException;
+import java.io.InputStream;
+import java.util.Properties;
+
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+import de.word_light.gateway.config.ApplicationInitializer;
 
 
 @SpringBootApplication
@@ -9,7 +16,34 @@ public class GatewayApplication {
 
 	public static void main(String[] args) {
 
-        // new ApplicationInitializer(args).init();
+        new ApplicationInitializer(args).init();
 		SpringApplication.run(GatewayApplication.class, args);
 	}
+
+            
+    /**
+     * @return the version of this api from {@code build.gradle} or and empty String if 'version' prop is not found
+     * 
+     * @since 0.0.9
+     */
+    public static String getApiVersion() {
+
+        String fileName = "build.gradle";
+        String propName = "version";
+
+        try (InputStream in = new FileInputStream(fileName)) {
+            Properties props = new Properties();
+            props.load(in);
+            Object versionProp = props.get(propName);
+
+            String version = versionProp != null ? versionProp.toString().replace("'", "") 
+                                                 : "Failed to get version. Could not find '" + propName + "' attribute in '" + fileName + "' file.";
+
+            return version;
+
+        } catch (IOException e) {
+            e.printStackTrace();
+            return "Failed to get version. Could not find file '" + fileName + "'";
+        }
+    }
 }
